f126a7491a67b31eff77b0214f238bfa1cc82e03
Sam Bukowski <sam.c.bukowski@gmail.com>
2024-04-29 13:08:23 -0600

    update protos for tx creation

--- internal/chain/transaction.go
+++ internal/chain/transaction.go
@@ -3,15 +3,21 @@ package chain
 import (
 	"context"
 	"crypto/ed25519"
-	"encoding/binary"
+	"encoding/hex"
+	"fmt"
 	"math/big"
 
-	primproto "buf.build/gen/go/astria/astria/protocolbuffers/go/astria/primitive/v1"
-	sqproto "buf.build/gen/go/astria/astria/protocolbuffers/go/astria/sequencer/v1"
-	client "github.com/astriaorg/go-sequencer-client/client"
+	primproto "buf.build/gen/go/astria/primitives/protocolbuffers/go/astria/primitive/v1"
+	txproto "buf.build/gen/go/astria/protocol-apis/protocolbuffers/go/astria/protocol/transactions/v1alpha1"
+	"github.com/astriaorg/go-sequencer-client/client"
 	"github.com/cometbft/cometbft/libs/bytes"
 
 	"github.com/ethereum/go-ethereum/common"
+)
+
+const (
+	// DefaultSequencerNetworkId is the default network id for the sequencer.
+	DefaultSequencerNetworkId = "astria-dusk-5"
 )
 
 type TxBuilder interface {
@@ -55,21 +61,30 @@ func (b *TxBuild) Transfer(ctx context.Context, to string, value *big.Int) (byte
 	buf := make([]byte, 16)
 	value.FillBytes(buf)
 
-	leastSignificant64 := binary.BigEndian.Uint64(buf[8:])
-	mostSignificant64 := binary.BigEndian.Uint64(buf[:8])
+	amount, err := convertToUint128(value)
+	if err != nil {
+		panic(err)
+	}
 
-	toAddress := common.HexToAddress(to)
-	unsignedTx := &sqproto.UnsignedTransaction{
-		Nonce: nonce,
-		Actions: []*sqproto.Action{
+	toBytes, err := hex.DecodeString(to)
+	if err != nil {
+		panic(err)
+	}
+	toAddr := &primproto.Address{
+		Inner: toBytes,
+	}
+
+	unsignedTx := &txproto.UnsignedTransaction{
+		Params: &txproto.TransactionParams{
+			ChainId: DefaultSequencerNetworkId,
+			Nonce:   nonce,
+		},
+		Actions: []*txproto.Action{
 			{
-				Value: &sqproto.Action_TransferAction{
-					TransferAction: &sqproto.TransferAction{
-						To: toAddress.Bytes(),
-						Amount: &primproto.Uint128{
-							Lo: leastSignificant64,
-							Hi: mostSignificant64,
-						},
+				Value: &txproto.Action_TransferAction{
+					TransferAction: &txproto.TransferAction{
+						To:         toAddr,
+						Amount:     amount,
 						AssetId:    client.DefaultAstriaAssetID[:],
 						FeeAssetId: client.DefaultAstriaAssetID[:],
 					},
@@ -85,4 +100,28 @@ func (b *TxBuild) Transfer(ctx context.Context, to string, value *big.Int) (byte
 	result, err := b.sequencerClient.BroadcastTxSync(ctx, signedTx)
 
 	return result.Hash, err
+}
+
+// convertToUint128 converts a string to a Uint128 protobuf
+func convertToUint128(numStr *big.Int) (*primproto.Uint128, error) {
+	bigInt := new(big.Int)
+
+	// check if the number is negative or overflows Uint128
+	if bigInt.Sign() < 0 {
+		return nil, fmt.Errorf("negative number not allowed")
+	} else if bigInt.BitLen() > 128 {
+		return nil, fmt.Errorf("value overflows Uint128")
+	}
+
+	// split the big.Int into two uint64s
+	// convert the big.Int to uint64, which will drop the higher 64 bits
+	lo := bigInt.Uint64()
+	// shift the big.Int to the right by 64 bits and convert to uint64
+	hi := bigInt.Rsh(bigInt, 64).Uint64()
+	uint128 := &primproto.Uint128{
+		Lo: lo,
+		Hi: hi,
+	}
+
+	return uint128, nil
 }
